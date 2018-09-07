12137eff459d8d6748b109963d20e757040c6e33
Bradley Schafer <schaferbradley@gmail.com>
2018-09-07 00:54:24 -0400

    Up to lecture 44.

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour 
+{
+  private Rigidbody _rigidBody;
+
+  // Use this for initialization
+  void Start () 
+  {
+    _rigidBody = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update () 
+  {
+    ProcessInput();
+  }
+
+  private void ProcessInput()
+  {
+    if (Input.GetKey(KeyCode.Space))
+    {
+      _rigidBody.AddRelativeForce(Vector3.up);
+    }
+
+    if (Input.GetKey(KeyCode.A))
+      print("Rotating Left");
+    else if (Input.GetKey(KeyCode.D))
+      print("Rotating Right");
+  }
+}

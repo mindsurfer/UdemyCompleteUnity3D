0b1f168af8d0468e49bbdbbc993a68a780502a79
Bradley Schafer <schaferbradley@gmail.com>
2018-09-17 01:10:39 -0400

    Periodic commit... Heading into cross platform input chapter.

---
+++ Argon Assault/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour 
+{
+
+  // Use this for initialization
+  void Start () 
+  {
+    var loadFirstLevel = StartCoroutine(LoadFirstLevel());
+  }
+
+  public void LoadLevel(int levelIndex)
+  {
+    SceneManager.LoadScene(levelIndex);
+  }
+
+  private IEnumerator LoadFirstLevel()
+  {
+    yield return new WaitForSecondsRealtime(3f);
+    LoadLevel(1);
+  }
+}
---
+++ Argon Assault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour 
+{
+  private void Awake()
+  {
+    var musicPlayerCount = FindObjectsOfType<MusicPlayer>().Length;
+    if (musicPlayerCount == 1)
+    {
+      print("created");
+      DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+      print("destroyed");
+      Destroy(gameObject);
+    }
+  }
+
+  // Use this for initialization
+  void Start () 
+  {
+    
+  }
+
+  // Update is called once per frame
+  void Update () 
+  {
+    
+  }
+}
